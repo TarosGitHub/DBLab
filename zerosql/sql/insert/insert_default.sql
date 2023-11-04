008c46409de078254da4e5915bfb71d23858c789
Taro Hoshino <no.taro1990@gmail.com>
2023-11-04 18:09:45 +0900

    Add SQL of inserting

---
+++ zerosql/sql/insert/insert_default.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    ShohinIns (shohin_id, shohin_mei, shohin_bunrui, hanbai_tanka, shiire_tanka, torokubi)
+VALUES
+    ('0007', 'おろしがね', 'キッチン用品', DEFAULT, 790, '2009-04-28')
+;
+
+/*
+Note:
+デフォルト値が設定されていない列を省略すると、その列にはNULLが代入される。
+*/
