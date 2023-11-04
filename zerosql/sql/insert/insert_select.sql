5e6ef55e8714726f71c7ab05ddcdf2b4421f570c
Taro Hoshino <no.taro1990@gmail.com>
2023-11-04 18:19:46 +0900

    Add SQL of insert select

---
+++ zerosql/sql/insert/insert_select.sql
@@ -0,0 +1,19 @@
+INSERT INTO ShohinCopy
+(
+    shohin_id,
+    shohin_mei,
+    shohin_bunrui,
+    hanbai_tanka,
+    shiire_tanka,
+    torokubi
+)
+SELECT
+    shohin_id,
+    shohin_mei,
+    shohin_bunrui,
+    hanbai_tanka,
+    shiire_tanka,
+    torokubi
+FROM
+    Shohin
+;
