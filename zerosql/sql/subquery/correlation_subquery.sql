55461945d3e42750ee93c5952f596564ef532eb7
Taro Hoshino <no.taro1990@gmail.com>
2024-01-27 16:29:26 +0900

    Add correlation subquery

---
+++ zerosql/sql/subquery/correlation_subquery.sql
@@ -0,0 +1,10 @@
+/* 相関サブクエリ */
+SELECT shohin_bunrui, shohin_mei, hanbai_tanka
+FROM Shohin AS S1
+WHERE hanbai_tanka > (
+    SELECT AVG(hanbai_tanka)
+    FROM Shohin AS S2
+    WHERE S1.shohin_bunrui = S2.shohin_bunrui /* バインド */
+    GROUP BY shohin_bunrui
+)
+;
